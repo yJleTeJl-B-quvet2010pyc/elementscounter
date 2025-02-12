ee9901c9b8b99622b48f337ca814e406a2ea5d4b
Yaroslav Andreev <yar.andreev77@gmail.com>
2025-02-12 09:52:57 +0300

    Counter add

---
+++ src/Main.groovy
@@ -0,0 +1,15 @@
+static void main(String[] args) {
+  def result = countElements(arrInit())
+  println("Результат: " + result)
+}
+
+static def countElements(List list) {
+  list.countBy { it }
+}
+
+static def arrInit() {
+  println ("Введите значения: ")
+  Scanner scanner = new Scanner(System.in);
+  String input = scanner.nextLine()
+  List<Integer> inputList = input.split(" ").collect { it.toInteger() }
+}
